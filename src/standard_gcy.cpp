c387d7fbb36100ec12ab12f54559c318643569a5
gcy3answer <2791544265@qq.com>
2026-03-27 18:00:11 +0800

    Update standard_gcy.cpp

--- src/standard_gcy.cpp
+++ src/standard_gcy.cpp
@@ -25,10 +25,22 @@
 #include "tools/plotter.hpp"
 #include "tools/recorder.hpp"
 
+const std::string keys =
+  "{help h usage ? |      | 输出命令行参数说明}"
+  "{@config-path   | configs/standard3.yaml | 位置参数，yaml配置文件路径 }";
 
 
-int main() {
+
+int main(int argc, char * argv[]) {
   fmt::print("Hello from standard_gcy inside Dev Container!\n");
+
+  cv::CommandLineParser cli(argc, argv, keys);
+  auto config_path = cli.get<std::string>(0);
+  if (cli.has("help") || config_path.empty()) {
+    cli.printMessage();
+    return 0;
+  }
+
 
   tools::Exiter exiter;
   tools::Plotter plotter;
@@ -80,7 +92,6 @@ int main() {
     
    return 0;
 }
-   
 
     
  
